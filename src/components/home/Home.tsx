ef696d7f9e1e9784668d7873575d3624ed08b61c
Istvan <17463906+purple0CaT@users.noreply.github.com>
2022-01-20 09:10:34 +0000

    img fix?

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -4,29 +4,11 @@ import "./Home.css";
 function Home() {
   return (
     <div className="homeWrapper">
-      {/* <Parallax
-        className="h-100"
-        bgImage="/bgImg.jpg"
-        strength={400}
-        bgImageStyle={{ minHeight: "calc( 100vh - 1px )" }}
-      >
-        Content goes here. Parallax height grows with content height.
-      </Parallax> */}
-      {/* <Parallax
-        // className={style.text}
-        // offsetYMax={70}
-        // offsetYMin={-70}
-        // offsetXMax={-30}
-        // offsetXMin={30}
-        x={[100, -100]}
-      >
-        <span className="h1">Horizontal</span>
-      </Parallax> */}
       <ParallaxBanner
         className="your-class"
         layers={[
           {
-            image: "/bgImg.jpg",
+            image: "bgImg.jpg",
             amount: 0.15,
           },
         ]}
