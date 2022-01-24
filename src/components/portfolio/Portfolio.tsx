83c7a15e90294683127a93b2716dca79cb760b1a
Istvan <17463906+purple0CaT@users.noreply.github.com>
2022-01-24 09:41:26 +0000

    update transition portfolio section

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,4 +1,5 @@
 import { useInView } from "react-intersection-observer";
+import { Parallax } from "react-scroll-parallax";
 import PortfolioData from "../../data/portfolioData.json";
 import PortfolioItem from "./PortfolioItem";
 import "./style/Port.scss";
@@ -6,6 +7,7 @@ import "./style/Port.scss";
 function Portfolio() {
   const { ref, inView, entry } = useInView({
     threshold: 0.8,
+    triggerOnce: true,
   });
   //
   return (
@@ -14,7 +16,7 @@ function Portfolio() {
         <h1 className="mx-auto my-auto">Portfolio</h1>
       </div>
       <section ref={ref} className={`portfolioItemsWrapper`}>
-        <ul
+        {/* <ul
           className={`portfolioItemsContainer shadowsBox ${
             inView && "portfolioAppear"
           }`}
@@ -22,7 +24,16 @@ function Portfolio() {
           {PortfolioData.map((item, index): any => (
             <PortfolioItem item={item} index={index} key={index + "asd"} />
           ))}
-        </ul>
+        </ul> */}
+        <Parallax className="h-100" x={[-40, 55]} tagOuter="figure">
+          <div className="portfolioItemsContainerWrapper shadowsBox">
+            <ul className={`portfolioItemsContainer ${inView && "portfolioAppear"}`}>
+              {PortfolioData.map((item, index): any => (
+                <PortfolioItem item={item} index={index} key={index + "asd"} />
+              ))}
+            </ul>
+          </div>
+        </Parallax>
       </section>
     </main>
   );
